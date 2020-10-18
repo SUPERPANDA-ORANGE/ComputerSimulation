be2f9c3ae8185189d38de8d5cf9b339e6f11131c
张午阳 <callmemaybe@tongji.edu.cn>
2020-10-18 12:21:42 +0800

    FirstCommit
    将一个旧项目直接上传

---
+++ New Unity Project/Assets/Script/RubeController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class RubeController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+       // QualitySettings.vSyncCount = 0;
+       // Application.targetFrameRate = 10;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float hor = Input.GetAxis("Horizontal");
+        float ver = Input.GetAxis("Vertical");
+        Debug.Log(hor);
+        Vector2 pos = transform.position;
+        pos.x = pos.x + 3.0f * hor*Time.deltaTime;
+        pos.y = pos.y + 3.0f * ver*Time.deltaTime;
+        transform.position = pos;
+    }
+}
